1d2a19599b50da7e2f56ee8ab563075e2fb2f420
Emily <emilyahmad77@gmail.com>
2026-03-12 13:23:52 -0400

    practice w tutorials, no real changes yet

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import {createRoot, useState} from 'react-dom/client'
 
+import { name, age } from "./person.js"; // destructure
+import message from "./message.js";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <button onClick={() => setCount(count + 1)}>
+      Count: {count}
+    </button>
+  );
+}
+
+// createRoot() defines HTML element & displays
+// render() what is rendered in HTML container
+// result displayed in root element
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <Counter />
+);
---
+++ src/person.js
@@ -0,0 +1,33 @@
+export const name = "Tobias"
+export const age = 18
+
+export {name, age}
+
+// template strings
+const name = "John";
+const age = 30;
+const message = `Hello, ${name}!
+You are ${age} years old.`;
+
+const items = ["ap", "ban", "orange"]
+const list = `You have $[items.length] items:
+${items.map(item => `- ${item}`).join('\n')}`;
+
+function highlight(strings, fname) {
+    let x = fname.toUpperCase();
+    return strings[0] + x + strings[1];
+}
+
+let name0 = "John";
+let text = highlight`Hello ${name0}, how are you?`;
+
+function higlight(strings, fname1, fname2) {
+    let x = fname1.toUpperCase();
+    let y = fname2.toUpperCase();
+    return strings[0] + x + strings[1] + y + strings[2];
+}
+
+let name1 = "Johnny";
+let name1 = "Jane";
+
+let text1 = highlight`Hello ${name1} and ${name2}, how are you?`;
